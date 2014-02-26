ae13b584100320c707da73a1a608edee7470c795
tmanundercover <terrell.singleton@gmail.com>
2014-02-26 04:45:13 -0500

    requirejs configured

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,77 +1,27 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration or grunt.loadTasks('directory') to read ALL js files here
+  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    //this level of the config object correspons a grunt plugin
-    connect: {
-      dev: {
-        options: {
-          base: ['prod', 'src', '.'],
-          port: 8000,
-          hostname: '*',
-        }
-      },
-    },
-    clean: {
-      prod: 'prod',
-    },
-    copy: {
-      app: {
-        src: 'src/pages/index.html',
-        dest: 'prod/index.html',
-      },
-    },
-    'jshint': {
-    build: {
-        options: {
-          jshintrc: '.jshintrc'
-      },
-        src: ['Gruntfile.js', 'build/**/*.js']
-      },
-        app: {
-          options: {
-            jshintrc: 'src/.jshintrc'
-          },
-          src: ['src/**/*.js']
-        }
-    },
-    watch: {
-      livereload: {
-        options: {
-          livereload: true,
-        },
-        files: ['Gruntfile.js','src/**/*.{js,html}', 'prod/*'],
-        tasks: []
-      },
-      jshintrc: {
-        files: ['**/*/.jshintrc'],
-        tasks: ['jshint']
-      },
-      scripts: {
-        files: ['<%= jshint.app.src %>'],
-        tasks: ['jshint:app'],
-      },
-      page: {
-        files: 'src/pages/*.html',
-        tasks: ['copy:app'],
-      },
-    }
   });
 
   // Load Grunt plugins.
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadTasks('build');
 
   // Tasks.
   grunt.registerTask('dev',
     'Compile and start a dev webserver.',
-    ['jshint:build','clean:prod','copy:app', 'connect:dev', 'watch']);
-    
+    ['jshint', 'clean:prod', 'copy:app', 'server:dev', 'watch']);
+
+  grunt.registerTask('server',
+    'Start the REST server and static server.',
+    function() {
+      require('./server/server');
+      grunt.task.run('connect:' + this.args.join(':'));
+    }
+  );
+
   grunt.registerTask('default', ['dev']);
 
 };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,39 @@
+define(["jquery", "underscore", "backbone"], function($, _, Backbone) {
+  var Photo = Backbone.Model.extend({
+    urlRoot : function() {
+      return "http://localhost:8001/photos";
+    }
+  });
 
-function awesome() {
-  document.body.innerHTML = 'OMG T is awesome';
-}
+  var PhotoView = Backbone.View.extend({
+    model : Photo,
 
-awesome();
+    template: _.template($("#photo_view").text()),
+
+    render: function() {
+
+      this.$el.html(
+        this.template(
+          this.model.toJSON()
+        )
+      );
+
+      return this;
+    }
+  });
+
+  var me = new Photo({
+    id: 1
+  });
+
+
+  var view = new PhotoView({
+    model : me
+  });
+
+  view.$el.appendTo('body');
+
+  me.fetch().always(function() {
+    view.render();
+  });
+});
