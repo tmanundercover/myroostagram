c06daf37ea6622a0f6f6496c244e982da4a32d2a
tmanundercover <terrell.singleton@gmail.com>
2014-02-28 00:51:44 -0500

    layouts added

--- src/modules/core/router.js
+++ src/modules/core/router.js
@@ -4,13 +4,13 @@ define(function(require) {
 
   var Backbone = require('backbone');
 
-  var GalleryView = require('components/photo/gallery-view');
+  var OneColLayout = require('layouts/one-col');
+  var TwoColLayout = require('layouts/two-col');
 
   // Create and instantiate our photocollection - we're going to pass this to
   // every view, but we don't want to create it every single time - just
   // when we start our application
   var PhotoCollection = require('components/photo/collection');
-
   var photos = new PhotoCollection();
 
   // Defining the application router, you can attach sub routers here.
@@ -21,16 +21,26 @@ define(function(require) {
       "photo/:id": "singlePhoto"
     },
 
+    insertView: function(pageView) {
+      if (this.currentView) {
+        this.currentView.destroy();
+      }
+      this.currentView = pageView;
+      pageView.$el.appendTo("#photos");
+      pageView.render().place();
+    },
+
     index: function() {
+      this.insertView(new TwoColLayout({
+        collection: photos
+      }));
+    },
 
-      photos.fetch().then(function(){
-
-        new GalleryView({
-          collection: photos,
-          el: '#photos'
-        }).render().place();
-
-      });
+    singlePhoto: function(id) {
+      this.insertView(new OneColLayout({
+        collection: photos,
+        modelId: id
+      }));
 
     }
 
